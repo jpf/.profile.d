99035eb50ec72cba18cc6e37a74d1b19db2b6a1e
Joël Franusic <joel@twilio.com>
2012-09-12 13:42:11 -0400

    adding .emacs.d

---
+++ .emacs.d/init.el
@@ -0,0 +1,21 @@
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+
+;;; via: http://www.plope.com/Members/chrism/flymake-mode
+(when (load "flymake" t) 
+  (defun flymake-pyflakes-init () 
+    (let* ((temp-file (flymake-init-create-temp-buffer-copy 
+		       'flymake-create-temp-inplace)) 
+	   (local-file (file-relative-name 
+			temp-file 
+			(file-name-directory buffer-file-name)))) 
+      (list "pyflakes" (list local-file)))) 
+  
+  (add-to-list 'flymake-allowed-file-name-masks 
+	       '("\\.py\\'" flymake-pyflakes-init))) 
+
+(add-hook 'find-file-hook 'flymake-find-file-hook)
+(put 'downcase-region 'disabled nil)
+
+(load "color-theme-tomorrow")
+
+(color-theme-tomorrow-night)
